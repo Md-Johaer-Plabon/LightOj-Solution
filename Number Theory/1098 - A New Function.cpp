49775e39100b92ebea0e5a0d242ebeacbdc5da00
Md Johaer Plabon <mdplabonda@gmail.com>
2020-03-23 00:51:53 +0600

    Update 1098 - A New Function.cpp

--- Number Theory/1098 - A New Function.cpp
+++ Number Theory/1098 - A New Function.cpp
@@ -1,3 +1,36 @@
+/* Name : Johaer Plabon
+   Dept. of CSE,SEC.
+   11th batch(2017-18).
+
+   Problem Name : A New Function.
+   Solution Idea : The problem is,You will be given a number N,You have to print
+   total SOD from 1 to N. If we approach bruteforce for this problem, The programme
+   will be crashed away.Now see in below:
+   
+   1 = (The number is not divisior of itself according to statement.)
+   2 = "
+   3 = "
+   4 = 2
+   5 =
+   6 = 2 + 3
+   7 =
+   8 = 2 + 4
+   9 = 3
+  10 = 2 + 5
+ Our task is to sum all of these divisiors.If we see top right side divisiors they are:
+ 2,3,4,5.We can use the formula of series sum 2-> n/2 or 10/2=5.
+ 
+ There are few steps : A loop from 2 to √(10).
+ Step 1: Calculate sum 2 to 10/2 using series law. /// [m/2 * (2*a + (m-1)*d)].
+         Then calculate sum of extra 2 = (10/2 - 2)*2.
+
+ Step 2: Again Calculate sum of series from 3 to 10/3.
+         Then calculate sum of extra 3 = (10/3 - 3)*3.
+
+   Now the sum is CSOD of 10.Continue the process to √(n) of any to calculate CSOD(n).
+*/
+
+
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;

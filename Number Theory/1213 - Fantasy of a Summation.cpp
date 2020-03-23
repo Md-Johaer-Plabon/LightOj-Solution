d2f72754de4764912efac3b08759da5e6464fa31
Md Johaer Plabon <mdplabonda@gmail.com>
2020-03-23 11:13:49 +0600

    Update 1213 - Fantasy of a Summation.cpp

--- Number Theory/1213 - Fantasy of a Summation.cpp
+++ Number Theory/1213 - Fantasy of a Summation.cpp
@@ -1,3 +1,35 @@
+ 
+/* Name : Johaer Plabon
+   11th batch(2017-18),
+   Dept of CSE,SEC.
+   
+   Problem Name : Fantasy summation.
+   Solution Idea : The problem is,
+   
+                    
+         for( i1 = 0; i1 < n; i1++ ) {
+            for( i2 = 0; i2 < n; i2++ ) {
+                for( i3 = 0; i3 < n; i3++ ) {
+                    ...
+                    for( iK = 0; iK < n; iK++ ) {
+                        res = ( res + A[i1] + A[i2] + ... + A[iK] ) % MOD;
+                    }
+                    ...
+                }
+            }
+      To calculate the result.We can perform this work in two steps.
+
+    Step 1 : To calculate sum if we only add the value of last loop.
+             If n = no. Of element & k = no. Of loops then the sum will be for the nested
+    loop = n^(k-1) * sum.
+ 
+    Step 2 : Now we have to count the extra value who have added to the sum.
+    Ans is the sum of step1 will be added for extra k time.
+    So the result will be (sum * k)%mod.
+
+*/
+
+
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;

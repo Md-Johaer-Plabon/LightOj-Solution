68d5a25228a3165a83d8d16e941bc2d3558e44e2
Md Johaer Plabon <mdplabonda@gmail.com>
2020-04-13 02:25:25 +0800

    Update 1231 - Coin Change (I).cpp

--- Dynamic Programming/1231 - Coin Change (I).cpp
+++ Dynamic Programming/1231 - Coin Change (I).cpp
@@ -1,41 +1,49 @@
 
-/* Name : Johaer Plabon
+/*
+   Name : Johaer Plabon
    11th batch(2017-18),
    Dept of CSE,SEC.
+   
+   Problem Name: Coin Change (I).
+   Topic : Coin Changing (DP).
+   
  */
-
-
-
 
 
 #include<bits/stdc++.h>
 using namespace std;
 #define mod 100000007
 #define ll long long
-int main (){
-ll n,t,cs=0;
-cin>>t;
-while(t--){
- ll k,dp[1009]={0},coin[60],cm[60];
-  cin>>n>>k;
-  dp[0]=1;
- for(int i=0;i<n;i++)cin>>coin[i];
- for(int i=0;i<n;i++)cin>>cm[i];
- for(int j=0;j<n;j++){
- for(int i=k;i>=1;i--){
-   if(i>=coin[j]){
-   ll temp=0;
-   for(int s=0 ;s<cm[j];s++){
-    temp+=coin[j];
-   if(temp<=i)
-   dp[i] = (dp[i] %mod + dp[i - temp]%mod)%mod;
-}
-}
- 
-}
- 
-}
-cout<<"Case "<<++cs<<": "<<dp[k]<<endl;
-}
-return 0;
+int main ()
+{
+    ll n,t,cs=0;
+    cin>>t;
+    while(t--)
+    {
+        ll k,dp[1009]= {0},coin[60],cm[60];
+        cin>>n>>k;
+        dp[0]=1;
+        for(int i=0; i<n; i++)cin>>coin[i];
+        for(int i=0; i<n; i++)cin>>cm[i];
+        for(int j=0; j<n; j++)
+        {
+            for(int i=k; i>=1; i--)
+            {
+                if(i>=coin[j])
+                {
+                    ll temp=0;
+                    for(int s=0 ; s<cm[j]; s++) // Loop for limitations of coins.
+                    {
+                        temp+=coin[j];
+                        if(temp<=i)
+                            dp[i] = (dp[i] %mod + dp[i - temp]%mod)%mod;
+                    }
+                }
+
+            }
+
+        }
+        cout<<"Case "<<++cs<<": "<<dp[k]<<endl;
+    }
+    return 0;
 }

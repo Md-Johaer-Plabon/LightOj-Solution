6f4e2dc661513965c1702cbb4f4566408757ee6c
Md Johaer Plabon <mdplabonda@gmail.com>
2021-01-22 23:40:16 +0600

    Create 1002 - Country roads.cpp

---
+++ Graph Theory/1002 - Country roads.cpp
@@ -0,0 +1,42 @@
+/*
+Name : Joaher Plabon 
+Dept : CSE, Sylhet Engineering College
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int,int> >v[505]; 
+vector<int>dist(505,0); 
+void dijkastra(int n){
+	for(int i=0;i<=500;i++)dist[i]=INT_MAX; 
+	dist[n]=0; 
+	priority_queue<pair<int,int> , vector<pair<int,int> >,greater<pair<int,int> > >q; 
+	q.push({0,n});
+	while(!q.empty()){
+		int x=q.top().second;
+		q.pop(); 
+		for(int i=0;i<v[x].size();i++) 
+		   if(dist[v[x][i].first]>max(dist[x],v[x][i].second)) 
+		   dist[v[x][i].first]=max(dist[x],v[x][i].second), 
+		   q.push({dist[v[x][i].first],v[x][i].first});
+	}
+}
+int main() {
+	int t,cs=0; 
+	cin>>t;
+	while(t--){
+		int x,y,n,m,w; 
+		for(int i=0;i<=500;i++)v[i].clear();
+		cin>>n>>m;
+		for(int i=0;i<m;i++) 
+		cin>>x>>y>>w,v[x].push_back({y,w}),v[y].push_back({x,w}); 
+		int des;
+		cin>>des;
+		dijkastra(des); 
+		printf("Case %d:\n",++cs); 
+		for(int i=0;i<=n-1;i++) 
+		if(dist[i]==INT_MAX)printf("Impossible\n"); 
+		else printf("%d\n",dist[i]);
+	}
+	return 0;
+}
